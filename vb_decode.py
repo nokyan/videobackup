0e850d58c62da693e166ca1d0218a0369173d9b9
ManicRobot <manicrobot@protonmail.com>
2021-04-04 02:54:50 +0200

    Removed LAB color space as it's unused and doesn't even benefit error correction

--- vb_decode.py
+++ vb_decode.py
@@ -19,62 +19,6 @@ COLOR_PALETTES = {
     "2": [(0, 0, 0), (255, 255, 255)]
 }
 
-LAB_COLOR_PALETTE = []
-
-LAB = False
-
-
-def rgb_to_lab(input_color):
-   num = 0
-   RGB = [0, 0, 0]
-
-   for value in input_color :
-       value = float(value) / 255
-
-       if value > 0.04045 :
-           value = ((value + 0.055) / 1.055) ** 2.4
-       else :
-           value = value / 12.92
-
-       RGB[num] = value * 100
-       num = num + 1
-
-   XYZ = [0, 0, 0,]
-
-   X = RGB [0] * 0.4124 + RGB [1] * 0.3576 + RGB [2] * 0.1805
-   Y = RGB [0] * 0.2126 + RGB [1] * 0.7152 + RGB [2] * 0.0722
-   Z = RGB [0] * 0.0193 + RGB [1] * 0.1192 + RGB [2] * 0.9505
-   XYZ[0] = round(X, 4)
-   XYZ[1] = round(Y, 4)
-   XYZ[2] = round(Z, 4)
-
-   XYZ[0] = float( XYZ[ 0 ] ) / 95.047         # ref_X =  95.047   Observer= 2°, Illuminant= D65
-   XYZ[1] = float( XYZ[ 1 ] ) / 100.0          # ref_Y = 100.000
-   XYZ[2] = float( XYZ[ 2 ] ) / 108.883        # ref_Z = 108.883
-
-   num = 0
-   for value in XYZ :
-
-       if value > 0.008856 :
-           value = value ** (0.3333333333333333)
-       else :
-           value = (7.787 * value) + (16 / 116)
-
-       XYZ[num] = value
-       num = num + 1
-
-   Lab = [0, 0, 0]
-
-   L = (116 * XYZ[1]) - 16
-   a = 500 * (XYZ[0] - XYZ[1])
-   b = 200 * (XYZ[1] - XYZ[2])
-
-   Lab [0] = round(L, 4)
-   Lab [1] = round(a, 4)
-   Lab [2] = round(b, 4)
-
-   return Lab
-
 
 def extract_frames(input_file: str):
     print("Using ffmpeg to extract every frame of the video.")
@@ -238,11 +182,6 @@ if __name__ == "__main__":
     
     if str(args.color_palette) not in COLOR_PALETTES.keys():
         raise ValueError("Invalid color palette size! Use one of these values: %s." % str(list(COLOR_PALETTES.keys())))
-    
-    if LAB:
-        print("Preparing error correction.")
-        for i in COLOR_PALETTES[str(COLOR_PALETTE_SIZE)]:
-            LAB_COLOR_PALETTE.append(rgb_to_lab(i))
     
     print("Starting decoding process.")
     
